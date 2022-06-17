dcb789cfa1a2a1754d27eca2af9d3f09cc5d45be
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-06-16 18:38:22 -0700

    User profiles and registration- front and back

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('server...;')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+//  Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the support desk API'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
